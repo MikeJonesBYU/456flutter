36725f7585e55db3f724279f2613827025a01df7
Mike Jones <jones@cs.byu.edu>
2019-12-23 12:58:56 -0700

    now does hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blueGrey[800],
-          appBar: AppBar(
-            title: Text('I am richest'),
-            backgroundColor: Colors.blueGrey[900],
-          ),
-          body: Center(
-            child: Image(
-              image: AssetImage('images/place-holder-1.jpg'),
-            ),
-          ),
-        )
-    ),
-  );
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.blueGrey[100],
+      appBar: AppBar(
+        title: Text('I am richest by far'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/place-holder-1.jpg'),
+        ),
+      ),
+    ));
+  }
+}
